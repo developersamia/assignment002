90b0cdc37a3b916a7a93b010c8bb16d08c912354
developersamia <samiatrisha75@gmail.com>
2025-07-25 19:56:37 +0530

    uploaded all files of my assignment from localhost
    uploaded all files of my assignment from localhost

---
+++ tailwind.cnfig.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        noto: ['Noto Serif', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"]
